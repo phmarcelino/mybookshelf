ab0990c5d29c2d52da5fef593d7b54284df38809
Pedro Henrique Marcelino <P3dro.h@hotmail.com>
2022-06-22 14:55:53 -0300

    Código SQL

--- SQL/create_table_book.sql
+++ SQL/create_table_book.sql
@@ -13,3 +13,16 @@ description varchar(1000),
 
 foreign key (id_user) references user (id)
 );
+select * from book;
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', 'Mentirosos', 'E.Lockhart', '256', 'Romance,FicÃ§Ã£o,Distopia,Literatura', 'NÃ£o-Nacional', '../img/62b0c6584dea8.jpg', 'Seguinte', 'Qualquer DescriÃ§Ã£o');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+insert into book (id_user, title, author, pages, genre, publi, cape, company, description) values ('1', '1984', 'George Orwell', 416, 'AÃ§Ã£o,FicÃ§Ã£o', 'Nacional', '../img/62b0c64746448.jpg', 'Companhia das Letras', 'Livro de George Orwell, romance que retrata uma distopia de 1984.');
+
